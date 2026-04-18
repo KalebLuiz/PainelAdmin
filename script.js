3923aedab0c25bdae54fccd8d0c7e6646d9673ee
Kaleb <kalebluiz2310@gmail.com>
2026-04-18 19:54:43 -0300

    commit

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function trocartela() {
-    const papeis = document.getElementById('blocktabela-papeis').style.display = 'none';
-    const selecionarpapeis = document.getElementById('block-novopapel').style.display = 'block';
 
-    papeis.style.display = 'none';
-    selecionarpapeis.style.display = 'block';
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
+let btn4 = document.querySelector(".btn4");
+let BlockTabelaPapeis = document.querySelector(".blocktabela-papeis")
+let BlockNovoPapel = document.querySelector(".block-novopapel")
 
-}
+btn3.addEventListener("click", function(){
+    BlockTabelaPapeis.style.display = 'none'
+    BlockNovoPapel.style.display = 'block';
+
+})
+
