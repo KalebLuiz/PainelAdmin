abdfae4b285eba6c7a81e329f74afc123b850412
Kaleb <kalebluiz2310@gmail.com>
2026-04-18 15:15:28 -0300

    COMMIT 18-04

---
+++ script.js
@@ -0,0 +1,8 @@
+function trocartela() {
+    const papeis = document.getElementById('blocktabela-papeis').style.display = 'none';
+    const selecionarpapeis = document.getElementById('block-novopapel').style.display = 'block';
+
+    papeis.style.display = 'none';
+    selecionarpapeis.style.display = 'block';
+
+}
