df2a3f29439eedc46ef4696b00382150f1bfcde4
Kaleb Luiz <kalebluiz2310@gmail.com>
2026-06-16 22:52:54 -0300

    .

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    API_LOGIN: "https://x8ki-letl-twmt.n7.xano.io/api:yNjbB5PN/auth/login",
+    API_PAPEL: "https://x8ki-letl-twmt.n7.xano.io/api:bQLUlOV-/papel",
+    API_PRODUTO: "https://x8ki-letl-twmt.n7.xano.io/api:bQLUlOV-/produto",
+    API_SINGUP: "https://x8ki-letl-twmt.n7.xano.io/api:zh4DA7Of/auth/signup",
+    API_AUTHME: "https://x8ki-letl-twmt.n7.xano.io/api:yNjbB5PN/auth/me",
+    API_USER: "https://x8ki-letl-twmt.n7.xano.io/api:bQLUlOV-/user"
+}
